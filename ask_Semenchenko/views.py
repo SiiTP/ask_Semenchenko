ae4d097c3c3c9fdab1e8e7de2421efca2c15a579
root <reg.lo@yandex.ru>
2015-04-20 22:15:42 +0300

    work with templates, static

--- ask_Semenchenko/views.py
+++ ask_Semenchenko/views.py
@@ -1,32 +1,50 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def hello(request):
-	#render(request,'picture.html',{})
-	return HttpResponse("Hello world! from views.py")
+    # render(request,'picture.html',{})
+    return HttpResponse("Hello world! from views.py")
+
 
 def postget(request):
-	outp =  """
-		<form action = "/postget/" method="post">
-			<label for="hello">Say hello!!!: </label>
+    outp = """
+        form action = "/postget/" method="post">
+            <label for="hello">Say hello!!!: </label>
 			<input id="hello" type="str" name="hello" value=""><br>
-			<button type="submit"> 
+        	<button type="submit">
 				OK
 			</button>
-	    	</form>
+	    </form>
 		%s
 		"""
-	if request.method == 'POST':
-		param = request.POST['hello']
-		outp = outp % ("You said :  " + str(param) + '\n')
-		return HttpResponse(outp)
-	return HttpResponse(outp)
+    if request.method == 'POST':
+        param = request.POST['hello']
+        outp = outp % ("You said :  " + str(param) + '\n')
+        return HttpResponse(outp)
+    return HttpResponse(outp)
+
 
 def myHello(request):
-	if request.method =='GET':
-		param=request.GET.get('param')
-		return HttpResponse("Get " + str(param))
+    if request.method == 'GET':
+        param = request.GET.get('param')
+        return HttpResponse("Get " + str(param))
+
 
 def main(request):
-	return render(request,'main.html',{})
-	
+    #if request.method == 'GET':
+    #    regVariant = request.GET.get('regVariant')
+    #    context = {'regVariant' : regVariant}
+    return render(request, 'main.html', {})
+
+
+def ask(request):
+    return render(request, 'ask.html', {})
+
+
+def question(request):
+    return render(request, 'question.html', {})
+
+
+def image(request):
+    return render(request, 'image.html', {})
